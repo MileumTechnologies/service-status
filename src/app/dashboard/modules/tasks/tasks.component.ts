5304d3b0dec55f5300d4e2a5be0b705f98058f0c
Ante <ante.adamovic93@gmail.com>
2018-06-19 09:26:06 +0200

    Add: Routing on tab change

--- src/app/dashboard/modules/tasks/tasks.component.ts
+++ src/app/dashboard/modules/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tasks-component',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styles: ['mat-form-field { margin-right: 25px; }']
 })
 export class TasksComponent {
+  public selectedIndex: number;
 
+  constructor(private router: Router) {
+    if (this.router.url.endsWith('create')) {
+      this.selectedIndex = 1;
+    } else if (this.router.url.endsWith('list')) {
+      this.selectedIndex = 0;
+    } else {
+      this.router.navigateByUrl('tasks/list');
+    }
+  }
+
+  public checkSelectedIndex(index: number): void {
+    console.log('lol');
+    if (index === 0 && !this.router.url.endsWith('list')) {
+      this.router.navigateByUrl('tasks/list');
+    } else if (index === 1 && !this.router.url.endsWith('create')) {
+      this.router.navigateByUrl('tasks/create');
+    }
+  }
 }
